330935e567ed85b470438a36302c4feff81cdbb4
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-01-22 01:48:59 -0600

    Day 16 -- part 2!

--- Day 16/day16.py
+++ Day 16/day16.py
@@ -1,13 +1,23 @@
 import sys
-import os
+import tqdm
 import numpy as np
+
 
 class Day16:
     def __init__(self, filename: str = None) -> None:
         if filename == None: return
+        self.filename = filename
         with open(filename) as f:
-            self.grid = np.array([list(line.strip()) for line in f.readlines()])
-            self.energized = np.array([[0]*len(self.grid[0]) for _ in range(len(self.grid))])
+            self.oldgrid = np.array([list(line.strip()) for line in f.readlines()])
+            self.grid = self.oldgrid.copy()
+            self.oldEnergy = np.array([[0]*len(self.grid[0]) for _ in range(len(self.grid))])
+            self.energized = self.oldEnergy.copy()
+            self.visited = [[[(0, 0)] for _ in range(len(self.grid[0]))] for _ in range(len(self.grid))]
+    
+    def reset(self) -> None: # CRAZY OVERHEAD ON HERE FOR EACH ITERATION...
+        with open(self.filename) as f:
+            self.grid = self.oldgrid.copy()
+            self.energized = self.oldEnergy.copy()
             self.visited = [[[(0, 0)] for _ in range(len(self.grid[0]))] for _ in range(len(self.grid))]
     
     def __str__(self) -> str:
@@ -22,12 +32,10 @@ class Day16:
         while 0 <= row < row_N and 0 <= col < col_N:
             # Check if this is a redundant motion:
             if (drow, dcol) in self.visited[row][col]:
-                print("DEAD END", row, col, self.visited[row][col])
                 return
             else:
                 self.visited[row][col].append((drow, dcol))
             
-            if row == 9 and col == 3: print("HERE")
             self.energized[row, col] = 1 # Energize this position
             pos = self.grid[row, col]
             # Turn if needed:
@@ -47,14 +55,35 @@ class Day16:
             row, col = row + drow, col + dcol
         return
 
-    def part1(self) -> int:
-        self.traverse(0, 0, 0, 1) # grid[0,0], going right
-        self.printEnergy()
-        return np.sum(self.energized)
+    def part1(self, row=0, col=0, drow=0, dcol=1) -> int:
+        self.traverse(row, col, drow, dcol) # grid[0,0], going right
+        total = np.sum(self.energized)
+        self.reset()
+        return total
+    
+    def traverseFromAllSides(self) -> int:
+        # Brute force, babey!
+        maxEnergy = 0
+        for row in tqdm.tqdm(range(len(self.grid))):
+            for col in range(len(self.grid[0])):
+                if row == 0 or row == len(self.grid)-1 or col == 0 or col == len(self.grid[0])-1:
+                    energy = max(self.part1(row, col, 1, 0), self.part1(row, col, -1, 0), self.part1(row, col, 0, 1), self.part1(row, col, 0, -1))
+                    if energy > maxEnergy:
+                        maxEnergy = energy 
+        return maxEnergy
+    
+    def part2(self) -> int:
+        return self.traverseFromAllSides()
+                
+            
+        
         
             
         
 if __name__ == "__main__":
     filename = sys.argv[1] if len(sys.argv) > 1 else "input2.txt"
     day16 = Day16(filename)
+    print("-----\nPart 1:")
     print(day16.part1())
+    print("-----\nPart 2:")
+    print(day16.part2())
