2be6305b92795a8f2de5e738cd49fe6277029666
Sam Johnson-Lacoss <99389146+samgjl@users.noreply.github.com>
2024-09-20 18:44:15 -0700

    Day 1 - part 1 - fin

--- sam-and-gwen/day_1/solution.cpp
+++ sam-and-gwen/day_1/solution.cpp
@@ -7,57 +7,52 @@ NEEDS:
 #include <fstream>
 #include <string>
 #include <vector>
-
 using namespace std;
+vector<string> converter = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
 int main(int argc, char *argv[])
 {
-    // if (argc < 2)
-    // {
-    //     cerr << "Please enter a file." << endl;
-    //     return -1;
-    // }
-    /* Algorithm:
-        1. Let total = 0
-        2. For each line:
-            2.a) Parse the integers from the line
-            2.b) Concatenate ints together to make one number
-            2.c) total += concatenated ints
-        3. return total
-    */
 
-    ifstream input_file("sample.txt");
+    ifstream input_file("sample.txt", ios::in);
     string line;
     int total = 0;
-    if (input_file.is_open()) {
+    if (!input_file.is_open())
+    {
         cerr << "File not found" << endl;
         return 0;
     }
     while (getline(input_file, line))
     {
-        cout << line << endl;
+        cout << line << " | ";
         string numbers = "";
         for (int i = 0; i < line.length(); i++)
         {
+            // TODO: Part 2 rewrite
+            // TODO: Do we read exclusively L->R or do we do a two pointer approach?
             if (isdigit(line[i]))
             {
                 numbers.push_back(line[i]);
+            } else if (false) {
+
             }
         }
+        int subtotal = 0;
         if (numbers.length() >= 2)
         {
             string sub = "";
             sub.push_back(numbers[0]);
             sub.push_back(numbers[numbers.length() - 1]);
-            total += stoi(sub);
+            subtotal = stoi(sub);
         }
-        else
+        else if (numbers.length() == 1)
         {
-            // parse into int, add to total
-            total += stoi(numbers);
+            subtotal = stoi(numbers + numbers);
         }
+        cout << subtotal << endl;
+        total += subtotal;
     }
     input_file.close();
 
-    cout << total << endl;    
+    cout << "Total: " << total << endl;
+
 }
